a823a44e3ea2c519c48e5c0b576f95e7ff5c1a83
89prasu <124338701+89prasu@users.noreply.github.com>
2024-01-18 11:36:07 +0530

    Update DButils.java

--- DButils.java
+++ DButils.java
@@ -3,7 +3,7 @@ Updating in stage branch for PR Demo..
 Helllo updated in stage branch May 5th
 updating in dev branch..
 
-Hello Everyone
+Hello all
 Git COmmands are very imp..
 dont miss these sessions..
 
